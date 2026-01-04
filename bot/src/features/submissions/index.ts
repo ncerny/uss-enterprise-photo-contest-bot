12b4c53ecbf8c9f67e5199af7347411c69014354
Nathan Cerny <ncerny@gmail.com>
2026-01-04 13:51:58 -0600

    feat: export submission management modules

---
+++ bot/src/features/submissions/index.ts
@@ -0,0 +1,8 @@
+export { SubmissionDeletionService, type DeletionOptions } from './submissionDeletionService';
+export { SubmissionManagementHandler } from './submissionManagementHandler';
+export { SubmissionCaptureHandler } from './captureService';
+export { SubmissionUploadService } from './uploadService';
+export { SubmissionPersistenceService } from './submissionPersistenceService';
+export { SubmissionFeedbackService } from './submissionFeedbackService';
+export { SubmissionLimitService } from './submissionLimitService';
+export { ContestSubmissionWatcher } from './messageWatcher';
